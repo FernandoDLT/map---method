2ac42fc69ea809958ec4b627d21c703bdabeff65
FernandoDLT <ferchodlt1971@gmail.com>
2023-11-02 14:47:51 -0500

    array-doubled

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ Array.prototype.myMap = function (callback) {
 const originalArray = [1, 2, 3, 4, 5];
 const squaredArray = originalArray.myMap((x) => x * x);
 console.log(squaredArray); // Output: [2, 4, 6, 8, 10]
+
+// Example 2: Doubling each element in an array
+const numbers = [2, 4, 6, 8, 10]
+const doubleNumbers = numbers.myMap((x) => x * 2)
+console.log(doubleNumbers)
